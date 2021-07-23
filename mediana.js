b674b8c94d987e339d6f7afd9f6c06a443ef3264
Carlos Sosa <carlos.sosa@x24.com.mx>
2021-07-23 17:09:11 -0500

    Avance hasta el 23 jul 17:09

--- mediana.js
+++ mediana.js
@@ -1,27 +1,35 @@
  const lista1 = [
-     100,
-     200,
      500,
-     400000000
+     100,
+     400000000,
+     200
  ];
 
- const mitadLista1 = parseInt(lista1.length / 2);
+function calcularMediana(lista) {
+    lista.sort(function (firstEl, secondEl) {
+        return firstEl - secondEl;
+    });
 
- function esPar(numero) {
+    const mitadLista = parseInt(lista.length / 2);
+
+    let mediana;
+
+    if(esPar(lista.length)){
+        const elemento1 = lista[mitadLista - 1];
+        const elemento2 = lista[mitadLista];
+
+        mediana = (elemento1 + elemento2) / 2;
+    } else {
+        mediana = lista[mitadLista];
+    }
+
+    return mediana;
+}
+
+function esPar(numero) {
     if(numero % 2 === 0) {
         return true;
     } else {
         return false;
     }
- }
-
- let mediana;
-
- if(esPar(lista1.length)){
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
-
-    mediana = (elemento1 + elemento2) / 2;
- } else {
-    mediana = lista1[mitadLista1];
- } 
+}
